d8c2c09becb610f2ba9108fafc51c8264b944ff3
Albert <67883068+quan1354@users.noreply.github.com>
2025-03-17 16:35:41 +0800

    '-'

--- lib/views_lab/networking.dart
+++ lib/views_lab/networking.dart
@@ -382,7 +382,9 @@ Future<Map<String, dynamic>> fetchUser() async {
     final response = await http.get(
       Uri.parse('https://d3v904dal0xey8.cloudfront.net/user/10000001'),
       // Send authorization headers to the backend if needed.
-      headers: {'Authorization': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImtpZCI6IjMifQ.eyJzdWIiOiIxMDAwMDAwMSIsImV4cCI6MTc0MTQ0NDMwNywibmJmIjoxNzQxNDQzNDA3LCJqdGkiOiJjZTc2MTA5OC03MzdkLTRiYWUtYTg5Ny0zYWMwNmZiZDE0NDkiLCJmYW1pbHkiOiJpaHVMR3p6STJKNGpKRjVhUkl2OVFBIiwic2NvcGUiOiJHRVRfL3VzZXIve3VzZXJfaWR9IFBVVF8vdXNlci97dXNlcl9pZH0gUE9TVF8vdXNlci9sb2dvdXQifQ.uSGeu5v2lScd4PjN6Q-8MJONfX3bYaLCQxidMW3YQF0'},
+      headers: {
+        'Authorization':
+            'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImtpZCI6IjMifQ.eyJzdWIiOiIxMDAwMDAwMSIsImV4cCI6MTc0MTUyOTU4MSwibmJmIjoxNzQxNTI4NjgxLCJqdGkiOiI3MzlhNGMzYi00ZDE3LTQ4YWYtYWJmNS02NDM0M2NkN2YxNTIiLCJmYW1pbHkiOiI2Z25LcXlPbS1Bd0dyX3dacjkxN3hRIiwic2NvcGUiOiJHRVRfL3VzZXIve3VzZXJfaWR9IFBVVF8vdXNlci97dXNlcl9pZH0gUE9TVF8vdXNlci9sb2dvdXQifQ.ej_RctbAE51yD4n6zBTP8xs7deUmRcTT23RLzv2V0wg'},
     );
 
     if (response.statusCode == 200) {
@@ -400,6 +402,46 @@ Future<Map<String, dynamic>> fetchUser() async {
     // Catch any exceptions (e.g., network errors, JSON parsing errors)
     // and rethrow with additional context.
     throw Exception('An error occurred: $e');
+  }
+}
+
+Future<Map<String, dynamic>> loginUser(String email, String password) async {
+  try {
+    // Define the URL for the login endpoint
+    final url = Uri.parse('https://d3v904dal0xey8.cloudfront.net/user/login');
+
+    // Define the headers, including the authorization token
+    final headers = {
+      'Authorization':
+          'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImtpZCI6IjMifQ.eyJzdWIiOiIxMDAwMDAwMSIsImV4cCI6MTc0MTUyOTU4MSwibmJmIjoxNzQxNTI4NjgxLCJqdGkiOiI3MzlhNGMzYi00ZDE3LTQ4YWYtYWJmNS02NDM0M2NkN2YxNTIiLCJmYW1pbHkiOiI2Z25LcXlPbS1Bd0dyX3dacjkxN3hRIiwic2NvcGUiOiJHRVRfL3VzZXIve3VzZXJfaWR9IFBVVF8vdXNlci97dXNlcl9pZH0gUE9TVF8vdXNlci9sb2dvdXQifQ.ej_RctbAE51yD4n6zBTP8xs7deUmRcTT23RLzv2V0wg','Content-Type': 'application/json',
+    };
+
+    // Define the body of the request
+    final body = jsonEncode({
+      'email_addr': email,
+      'password': password,
+    });
+
+    // Make the POST request
+    final response = await http.post(
+      url,
+      headers: headers,
+      body: body,
+    );
+
+    // Check the response status code
+    if (response.statusCode == 200) {
+      // If the server returns a 200 OK response, parse the JSON
+      return jsonDecode(response.body) as Map<String, dynamic>;
+    } else {
+      // If the server returns an error, throw an exception with the status code and response body
+      throw Exception(
+        'Failed to login. Status code: ${response.statusCode}, Response: ${response.body}',
+      );
+    }
+  } catch (e) {
+    // Catch any exceptions (e.g., network errors, JSON parsing errors) and rethrow with additional context
+    throw Exception('An error occurred during login: $e');
   }
 }
 
@@ -430,11 +472,24 @@ class _MyHomePageState extends State<MyHomePage> {
   Future<Map<String, dynamic>>? _futureUser;
   final TextEditingController _controller = TextEditingController();
 
+  void _performLogin() async {
+    try {
+      // Example usage of the loginUser function
+      final response =
+          await loginUser('ii887522@gmail.com', 'ii887522@gmail.com');
+      print(
+          'Login Response: ${jsonEncode(response)}'); // Print the entire JSON content
+    } catch (e) {
+      print('Error: $e');
+    }
+  }
+
   @override
   void initState() {
     super.initState();
     // Fetch the user data when the widget is initialized
     _futureUser = fetchUser();
+    _performLogin();
   }
 
   @override
